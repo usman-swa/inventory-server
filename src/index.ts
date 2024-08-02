e24907105b1d72e8db190230b48ac7bb2dfe1c41
Usman <usman.muhammad@fi.abb.com>
2024-08-02 16:18:59 +0300

    fist endpoint

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dashboardRoutes from "./routes/dashboardRoutes";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* ROUTE IMPORTS */
+
+
+
+
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
